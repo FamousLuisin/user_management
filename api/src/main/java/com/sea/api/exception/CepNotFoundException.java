d1052b436208f9ec340409588ed261b4ef1c289c
luis <lufizinmelo@gmail.com>
2026-04-05 21:46:26 -0300

    feat(api): add validations to viaCEP and add Jackson datetime support

---
+++ api/src/main/java/com/sea/api/exception/CepNotFoundException.java
@@ -0,0 +1,9 @@
+package com.sea.api.exception;
+
+public class CepNotFoundException extends RuntimeException {
+    
+    public CepNotFoundException(String message) {
+        super(message);
+    }
+    
+}
